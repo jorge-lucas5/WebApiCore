365f91617023f8aa2f7decf7b4e5d58ed126865b
jorgelucas <jorgelucasl91@gmail.com>
2021-03-29 17:08:40 -0300

    configurando AutoMapper

---
+++ src/Estudos.App.WebApi/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Estudos.App.Business.Models;
+using Estudos.App.WebApi.ViewModels;
+
+namespace Estudos.App.WebApi.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
