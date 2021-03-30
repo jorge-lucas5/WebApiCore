ba57619c71769320a628ce5a0aab80fc0d4a201a
jorgelucas <jorgelucasl91@gmail.com>
2021-03-29 22:16:02 -0300

    customizando response

--- src/Estudos.App.WebApi/Controllers/MainController.cs
+++ src/Estudos.App.WebApi/Controllers/MainController.cs
@@ -1,11 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Estudos.App.Business.Interfaces;
+using Estudos.App.Business.Notificacoes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Estudos.App.WebApi.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificador _notificador;
 
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+        protected ActionResult CustonResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
 
+            return CustonResponse();
+        }
+
+        private ActionResult CustonResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    sucesso = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                sucesso = false,
+                erros = _notificador.ObterNotificacoes().Select(n=> n.Mensagen)
+            });
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                var erroMessage = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+
+                NotificarErro(erroMessage);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
     }
 }
